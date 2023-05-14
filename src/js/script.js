e66e2fb7b5cba69c2ad19c2270d72b9da2bc76d7
Stepan <ssavdeyuk@edu.hse.ru>
2023-05-15 00:21:23 +0300

    fix header phone & logo links

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
 window.addEventListener('DOMContentLoaded', () => { 
 
     const burger = document.querySelector('.header__burger');
+
     const sideNav = document.querySelector('.header__sidemenu');
     // const sideNavClose = document.querySelector('.header__sidenav-close');
     const sideNavA = document.querySelectorAll('.header__sidemenu a');
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
         sideNav.classList.add('active');
         document.body.style.overflow = "hidden";
     })
+    
     // sideNavClose.addEventListener('click', () => {
     //     sideNav.classList.remove('active');
     //     document.body.style.overflow = "";
@@ -26,11 +28,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const popupWrapper = document.querySelector('.form__wrapper');
     const popupClose = document.querySelector('.form__close');
     const form = document.querySelector('.form form');
+    const headerPhone = document.querySelector('.header__phone-text');
+    const footerPhone = document.querySelector('.footer__phone-text');
 
     btnPopup.addEventListener('click', (e) => {
         e.preventDefault();
         popupWrapper.classList.add('active');
     })
+
+    headerPhone.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupWrapper.classList.add('active');
+    })
+
+    footerPhone.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupWrapper.classList.add('active');
+    })
+
 
     popupClose.addEventListener('click', (e) => {
         e.preventDefault();
